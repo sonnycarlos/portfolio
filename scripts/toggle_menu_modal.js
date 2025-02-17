e2c3ececba5d9daa6bb495ea35539e5a6e96c29e
sonnycarlos <135956466+sonnycarlos@users.noreply.github.com>
2025-02-17 16:41:15 +0800

    add menu modal for tablet and mobile view

---
+++ scripts/toggle_menu_modal.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('../components/menu_modal.html')
+    .then(response => response.text())
+    .then(html => {
+      document.querySelector('.menu_modal').innerHTML = html
+      
+      document.querySelector('.menu_modal .modal').style.display = 'none'
+
+      document.querySelector('.open_menu').addEventListener('click', function () {
+        document.querySelector('.menu_modal .modal').style.display = 'flex'
+      })
+
+      document.querySelector('.close_menu').addEventListener('click', function () {
+        document.querySelector('.menu_modal .modal').style.display = 'none'
+      })
+    })
+    .catch(error => console.error('Error loading the menu:', error))
+})
